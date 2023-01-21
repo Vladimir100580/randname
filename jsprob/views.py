cf15988c9ad13bc600b4b73b2c00a9d32f7f18b1
Vladimir <vovar3573@mail.ru>
2023-01-22 01:48:54 +0300

    'Progress2'

--- jsprob/views.py
+++ jsprob/views.py
@@ -18,7 +18,6 @@ def hello(request):
     else:
         nm = ''
         fl = 0
-
     f = 0
     if DataKlass.objects.filter(log=user_id).exists(): f = 1
 
@@ -26,7 +25,6 @@ def hello(request):
     if 'reg' in answer:
         return redirect('regist')
     if 'intlg' in answer:
-
         lg = answer.__getitem__('lg')
         if len(lg) < 3: return redirect('home')
         if User.objects.filter(username=lg).exists():
@@ -61,7 +59,6 @@ def regist(request):
     return render(request, 'jsprob/registr.html')
 
 
-
 def begin(request):
     print('Ку-ку')
 
@@ -69,9 +66,7 @@ def begin(request):
 def addkl(request):
     answer = request.GET
     user_id = request.user.username
-
     if (user_id == ''): return redirect('home')
-
     kls = DataKlass.objects.order_by('klass').filter(log=user_id).values_list('klass')
     st = ''
     if len(kls) > 0:
@@ -79,15 +74,13 @@ def addkl(request):
         for i in kls: st = st + i[0] + ', '
         st = st[:len(st) - 2] + '.'
 
-
     if 'kl' in answer:
         kl = answer.__getitem__('kl')
         if kl == '': return redirect('addkl')
         if DataKlass.objects.filter(klass=kl, log=user_id).exists():
             return render(request, 'jsprob/ujusekl.html')
-        DataKlass(log=user_id, klass=kl).save()
+        DataKlass(log=user_id, klass=kl, fios='', vyhods='', balls='').save()
         return redirect('addkl')
-
     return render(request, 'jsprob/addkl.html', {'kls': st})
 
 
@@ -102,7 +95,6 @@ def addu4(request):
     klasMas = [i[0] for i in kls]
     print('klasMas-', klasMas)
 
-
     if 'intlg' in answer:
         kl = answer.__getitem__('nomm')
         request.session['klas_use'] = kl
@@ -112,4 +104,56 @@ def addu4(request):
 
 
 def addu41(request):
-    print(1)
+    answer = request.GET
+    f = 0
+    user_id = request.user.username
+    if (user_id == ''): return redirect('home')
+    kl = request.session['klas_use']
+    klData = DataKlass.objects.filter(log=user_id, klass=kl).values_list('fios', 'vyhods', 'balls')
+    nms = klData[0][0].split('$}%*№')
+    vyhs = klData[0][1].split('$}%*№')
+    bals = klData[0][2].split('$}%*№')
+    fl = 1
+    while fl == 1:
+        fl = 0
+        for i in range(len(nms)-1):
+            if nms[i] > nms[i + 1]:
+                nms[i], nms[i + 1] = nms[i + 1], nms[i]
+                vyhs[i], vyhs[i + 1] = vyhs[i + 1], vyhs[i]
+                bals[i], bals[i + 1] = bals[i + 1], bals[i]
+                fl = 1
+    datnvb = []
+    for i in range(len(nms)): datnvb.append([str(i % 3), nms[i], vyhs[i], bals[i]])
+    if 'intfi' in answer:
+        fami = answer.__getitem__('famim').strip()
+        if len(fami) < 3: return redirect('addu41')
+        if DataKlass.objects.filter(log=user_id, klass=kl).exists() and fami in nms:
+            return render(request, 'jsprob/ujusefi.html', {'fami': fami, 'kl': kl})
+        nms.append(fami)
+        vyhs.append('0')
+        bals.append('0')
+        f = 1
+        # kar = DataKlass.objects.get(log=user_id, klass=kl)
+        # kar.fios = '$}%*№'.join(nms)
+        # kar.vyhods = '$}%*№'.join(vyhs)
+        # kar.balls = '$}%*№'.join(bals)
+        # kar.save()
+        # return redirect('addu41')
+    if 'delfi' in answer:
+        fami = answer.__getitem__('famim').strip()
+        if len(fami) < 3: return redirect('addu41')
+        if fami not in nms:
+            return render(request, 'jsprob/nofi.html', {'fami': fami, 'kl': kl})
+        i = nms.index(fami)
+        nms.pop(i)
+        vyhs.pop(i)
+        bals.pop(i)
+        f = 1
+    if f == 1:
+        kar = DataKlass.objects.get(log=user_id, klass=kl)
+        kar.fios = '$}%*№'.join(nms)
+        kar.vyhods = '$}%*№'.join(vyhs)
+        kar.balls = '$}%*№'.join(bals)
+        kar.save()
+        return redirect('addu41')
+    return render(request, 'jsprob/addu41.html', {'nms': datnvb})
